682236089c05073691f1bfff7c02b09a1513c172
matteo <matdeliperi.md@gmail.com>
2026-04-30 00:58:21 +0200

    addedo docker file for the entire projects

--- backend/app/core/tkq.py
+++ backend/app/core/tkq.py
@@ -1,14 +1,20 @@
+import os
 import taskiq_redis
 from taskiq import TaskiqScheduler
 from taskiq.schedule_sources import LabelScheduleSource
 from taskiq_redis import RedisScheduleSource, ListQueueBroker
+from dotenv import load_dotenv
 
-broker = ListQueueBroker("redis://localhost:6379/0")
+load_dotenv()
+
+redis_url = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+
+broker = ListQueueBroker(redis_url)
 
 scheduler = TaskiqScheduler(
     broker=broker,
     sources=[
-        RedisScheduleSource("redis://localhost:6379/0"),
+        RedisScheduleSource(redis_url),
         LabelScheduleSource(broker),
     ],
 )
